af8edc2a72c189ee38751440fe065a2c1a5dedd6
Valenzuela182 <rodrigo.valenzuela182@gmail.com>
2021-04-21 21:11:49 -0400

    rollback index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App';
+import App from './App';
 import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
